814ed46b8e30f3b39709321c6e8dcc9b224e6398
Daniel Paiva Salles (Emplacaki) <danielpaivasalles@gmail.com>
2022-09-05 11:49:55 -0300

    Comecei a fazer a base do algoritmo genético em .py

--- Brincadeiras/Otimização/algoritmo_genetico.py
+++ Brincadeiras/Otimização/algoritmo_genetico.py
@@ -1,12 +1,47 @@
 '''
 Autor: Daniel Paiva Salles
-Sintese: Utilizar a técnica de algoritmo genético para trabalhar a otimização deuma situação hipotética.
-Problema proposto: Uma empresa de emplacamento de veículos esta precisando reazlizar a compra de porcas, parafusos e 
-arruelas para repor o estoque. Os parafusos e arruelas são vendidos em pacotes fechados de 1kg e cada um possui um 
-determinado valor. A empresa disponibiliza de 500 reais para a aquisição destes insumos. Deve ser adiquirida ao menos 
-uma unidade de cada um dos itens citados, mas deve-se realizar a compra do maximo de itens possíveis respeitando o 
-valor maximo estabelecido e com a maior variedade possível. Porcas e parafusos não soberbos devem ser comprados juntos
-Itens:
-Parafuso fino, parafuso grosso, arruela fina, arruela grossa, parafuso fino soberbo, parafuso grosso soberbo, 
-porca parafuso fino, porca parafuso grosso
+Sintese: Utilizar a técnica de algoritmo genético para trabalhar a otimização de uma situação hipotética.
+Problema proposto: Uma empresa de emplacamento de veículos esta precisando realizar a compra de porcas, parafusos e 
+arruelas para repor o estoque. Todos os itens são vendidos em pacotes fechados e valores distintos, e todos os itens
+possuem suas versões finas e grossas. O valor dos itens finos são: Porcas(R$30,52), Parafusos(R$28,75), 
+Soberbos(R$29,63), Arruelas(R$26,74).
+Os itens grossos são: Porcas(R$36,13), Parafusos(R$34,19), Soberbos(R$35,48), Arruelas(R$31,32).
+A empresa disponibiliza de 500 reais para a aquisição destes insumos. Deve ser adiquirido todos os itens na maior
+quantidade possível, respeitando o dinheiro disponível e adiquirindo todos os itens. 
+A proporção de compra para porcas/arruelas/parafusos se da pela proporção seguinte proporção:
+Porca/Parafuso= 1:1
+Parafuso/Soberbo= 1:1
+Soberbo/Arruela= 1:1
+Parafuso/Arruela= 1:2
 '''
+import random
+
+#Etapas:
+# -Inicialização: Setar cromossomos aleatórios
+# -Medir a adaptação
+# -Elitismo
+# -Crossover
+# --Roleta
+# --Ponto de corte
+# -Mutação
+# -Ir pra proxima geração
+
+#Definições
+#Cada gene terá 8 cromossomos; Cada cromossomo pertencerá ao conjunto dos numeros NATURAIS; Os genes serão agrupados
+#em duas partes, os quatro primeiros cromossomos pertencerão ao conjunto fino e os quatro ultimos ao conjunto grosso;
+#A ordem dos cromossomos será, nos dois grupos, Parafuso(Pa), Porca(Po), Soberbo(So), Arruela(Ar); Cada população 
+#proposta como solução terá 7 genes; O mais bem adaptado será mantido por elitismo.
+
+#Etapa 0: Funções a serem utilizadas
+def cromos():
+    return random.randint(0, 50)
+
+#Etapa 1: Inicialização (Criar gene e setar cromossomos aleatórios)
+gene = []
+cromossomo = [cromos(), cromos(), cromos(), cromos(), cromos(), cromos(), cromos(), cromos()]
+for x in range(7):
+    gene.append(cromossomo)
+print(gene)
+
+#Etapa 2: Fitness (Medir a adaptação dos cromossomos)
+
